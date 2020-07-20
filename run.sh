8edb840d0775509bf360bf0e4d3d333c345b7a02
Thomas Trividic <thomas.trividic@prestashop.com>
2020-07-20 23:12:16 +0200

    run script ok

--- run.sh
+++ run.sh
@@ -7,4 +7,4 @@ gcloud compute instances create hello-world-instance \
     --metadata release-url=gs://ps_elixir_releases/gcp-0.1.0.tar.gz,release-name=gcp,release-version=0.1.0,startup-script-url=gs://ps_elixir_releases/instance-startup.sh \
     --zone europe-west1-c \
     --tags http-server,https-server \
-	--service-account=api-v2-storage@prestashop-data-integration.iam.gserviceaccount.com
+	--service-account=my-gcp-project.iam.gserviceaccount.com
