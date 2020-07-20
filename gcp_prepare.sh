a9486303ca962c40d0b79c2465883641f2532e11
Thomas Trividic <thomas.trividic@prestashop.com>
2020-07-20 22:09:32 +0200

    ok

---
+++ gcp_prepare.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+gcloud iam service-accounts create ps-elixir-releases \
+--project=my-gcp-project \
+--description="deploy elixir release" \
+--display-name="ps-elixir-releases"
+
+gcloud projects add-iam-policy-binding my-gcp-project \
+--member serviceAccount:ps-elixir-releases@my-gcp-project.iam.gserviceaccount.com \
+--role roles/storage.objectAdmin
